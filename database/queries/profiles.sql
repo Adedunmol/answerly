926dc45b6170034872a64c0ee0335c84ece62631
Adedunmola Oyewale <oyewaleadedunmola@gmail.com>
2026-01-01 21:53:46 +0100

    Add functionalities for profiles
    - Allow users get their profiles
    - Allow users update their profiles

--- database/queries/profiles.sql
+++ database/queries/profiles.sql
@@ -1,3 +1,19 @@
 -- name: CreateProfile :exec
 INSERT INTO profiles (user_id)
 VALUES (sqlc.arg(user_id));
+
+-- name: GetProfile :one
+SELECT * FROM profiles WHERE user_id = sqlc.arg(user_id);
+
+-- name: UpdateProfile :one
+UPDATE profiles
+SET
+    first_name = COALESCE(sqlc.narg(first_name), first_name),
+    last_name = COALESCE(sqlc.narg(last_name), last_name),
+    date_of_birth = COALESCE(sqlc.narg(date_of_birth), date_of_birth),
+    gender = COALESCE(sqlc.narg(gender), gender),
+    university = COALESCE(sqlc.narg(university), university),
+    faculty = COALESCE(sqlc.narg(faculty), faculty),
+    location = COALESCE(sqlc.narg(location), location)
+WHERE user_id = sqlc.arg(user_id)
+RETURNING *;
